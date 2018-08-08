c9128ff7cfbaa25c7cae78be5353c68c615cd2d9
cvantt <42219741+cvantt@users.noreply.github.com>
2018-08-08 17:19:31 -0400

    Data-Memory.v and updated Top.v

--- top.v
+++ top.v
@@ -6,12 +6,13 @@ reg [31:0]instruction, alu_input, adder_input; // pc input
 reg [1:0] mux_input;
 wire[31:0]adder_output, alu_output;
 wire[1:0] mux_output;
-
+PC m(adder_input, adder_output, alu_output, alu_input, instruction, mux_output, mux_input);
 
 
 
 reg [31:0]ReadAddress; //instruction cache input
 wire[31:0]decoder;  // output
+Instruction_Cache (ReadAddress, decoder);
 
 reg [31:0] instruction;
 reg  [4:0] r_one;//reg 1 for operand prep
@@ -40,30 +41,10 @@ reg [31:0]inputData;
 reg WriteData, ReadData;
 wire [31:0]Data;
 wire MissFlag;
-
-mux_input = 2'b00;		
-		assign mux_input = mux_output;
-		assign alu_input = alu_output;		
-		assign adder_input = adder_output;
-		instruction = 32'b1111_1000_0100_0000_0000_0010_1000_0001;
-		alu_input <= instruction;
-		adder_input <= instruction;
-end
-
-	initial begin
-		$monitor("Instruction value: %b, alu_output: %b, adder_output: %b, mux_out %b", instruction, alu_output, adder_output, mux_output);
-		end
-endmodule
+dataMemory dCache(Data, MissFlag, addr, inputData, WriteData, ReadData);
 
 
 
-Module Instruction_Cache (ReadAddress, decoder);
 
-initial begin
-assign ReadAddress = decoder;
-ReadAddress = c[0];
 
-	$monitor("Decoder value: %b", Decoder)
-end 
-endmodule
 
